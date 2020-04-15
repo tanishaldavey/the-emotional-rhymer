96978883063f56658183edff19fba069cd325e29
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-15 17:20:57 -0600

    Add basic structure for the search form

---
+++ src/containers/SearchForm/SearchForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './SearchForm.css';
+
+class SearchForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      queriedWord: ''
+    }
+  }
+  render() {
+    return(
+      <form>
+      </form>
+    )
+  }
+}
+
+
+export default SearchForm;

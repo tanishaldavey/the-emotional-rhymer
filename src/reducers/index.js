25e11bedbaeeaeebb4a13c89658da5787170307f
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-16 11:47:19 -0600

    Add wordDetails to combineReducers function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { rhymes } from './rhymes';
 import { queriedWord } from './queriedWord';
+import { wordDetails } from './wordDetails';
 
 const rootReducer = combineReducers({
   rhymes,
-  queriedWord
+  queriedWord,
+  wordDetails
 });
 
 export default rootReducer;

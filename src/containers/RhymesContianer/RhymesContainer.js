2b2ef72a1c527dde11c1037adbded2dafc937ceb
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-16 10:10:52 -0600

    Map contents of rhymes in state to an individual rhyme component

--- src/containers/RhymesContianer/RhymesContainer.js
+++ src/containers/RhymesContianer/RhymesContainer.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import Rhymes from '../../components/Rhymes/Rhymes';
 import './RhymesContainer.css';
 import { connect } from 'react-redux';
 
-const RhymesContainer = () => {
+const RhymesContainer = ({ rhymes }) => {
+  const allRhymes = rhymes.map(rhyme => {
+    return <Rhymes
+      key={rhyme.score}
+      rhyme={rhyme.word}
+    />
+  })
 
   return(
     <ul>
+      { allRhymes }
     </ul>
   )
 }
 
-const mapStateToProps = state =({
+const mapStateToProps = state => ({
   rhymes: state.rhymes
 })
 

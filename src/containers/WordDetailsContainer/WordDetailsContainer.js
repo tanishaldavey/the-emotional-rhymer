12a22a2ff876b88f1d7485bbcc955aef484f2a05
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-16 13:39:30 -0600

    Update the props being used to reflect the data being held in state

--- src/containers/WordDetailsContainer/WordDetailsContainer.js
+++ src/containers/WordDetailsContainer/WordDetailsContainer.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './WordDetailsContainer.css';
 
-const WordDetailsContainer = (props) => {
+const WordDetailsContainer = ({ wordDetails }) => {
   return(
     <section>
-    <p>I am here.</p>
-      <p>{props.wordDetails.partOfSpeech}</p>
-      <p>{props.wordDetails.definition}</p>
-      <p>Example of usage:</p>
-      <p>{props.wordDetails.examples}</p>
+    <h3>{wordDetails.word}</h3>
+    <p>{wordDetails.partOfSpeech}</p>
+    <p>{wordDetails.definition}</p>
+    <p>Example of usage:</p>
+    <p>{wordDetails.examples}</p>
     </section>
   )
 }

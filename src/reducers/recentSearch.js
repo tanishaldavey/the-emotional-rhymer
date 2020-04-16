b186676f4711caa03abb4da3931adc5b9b4ead52
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-16 17:47:45 -0600

    Remove nested if/else statement from overarching switch statement

--- src/reducers/recentSearch.js
+++ src/reducers/recentSearch.js
@@ -1,13 +1,8 @@
 export const recentSearches = (state =[], action) => {
   switch (action.type) {
     case 'GET_RECENT_SEARCHES':
-      if (state.length === 10) {
-        state.pop
-        return [action.recentSearchValue, ...state]
-      } else {
-        return [action.recentSearchValue, ...state]
-      };
+      return action.recentSearches;
     default:
       return state;
+    }
   }
-}

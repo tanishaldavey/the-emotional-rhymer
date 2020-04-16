ff8f910202883225246e08f4eab48237fc7a4b8c
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-16 11:49:58 -0600

    Connect component to store in order to dispatch information

--- src/components/Rhymes/Rhymes.js
+++ src/components/Rhymes/Rhymes.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { getWordDetails } from '../../actions';
+import { connect } from 'react-redux';
 import './Rhymes.css';
 
 const Rhymes = ({ rhyme }) => {
   return(
-    <li>{rhyme}</li>
+    <Link to='word-details'>
+      <li>{rhyme}</li>
+    </Link>
   )
 }
 
-export default Rhymes;
+const mapDispatchToProps = dispatch => ({
+  getWordDetails: details => dispatch( getWordDetails(details) )
+})
+
+export default connect(null, mapDispatchToProps)(Rhymes);

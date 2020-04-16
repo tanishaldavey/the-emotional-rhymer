4e4fdc6044bab7afe3315eb4691f4740a2c72523
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-16 17:46:35 -0600

    Add recentSearches to combineReducers function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { rhymes } from './rhymes';
 import { queriedWord } from './queriedWord';
 import { wordDetails } from './wordDetails';
+import { recentSearches } from './recentSearch';
 
 const rootReducer = combineReducers({
   rhymes,
   queriedWord,
-  wordDetails
+  wordDetails,
+  recentSearches
 });
 
 export default rootReducer;

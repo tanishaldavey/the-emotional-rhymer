d8609dd5ed609fdfa820906945af3621178b316a
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-16 08:26:07 -0600

    Create basic structure for the container

---
+++ src/containers/RhymesContianer/RhymesContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RhymesContainer.css';
+import { connect } from 'react-redux';
+
+const RhymesContainer = () => {
+  return(
+    <ul>
+    </ul>
+  )
+}
+
+export default RhymesContainer;

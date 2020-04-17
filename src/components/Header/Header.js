de2a580de0678c76ba4fe734066d361035d8892e
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-17 17:59:34 -0600

    Add text to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,8 @@ import './Header.css';
 const Header = () => {
   return(
     <header>
+      <h1>The Emotional Rhymer</h1>
+      <p>A rhyming dictionary for the emotional poet.</p>
     </header>
   )
 }

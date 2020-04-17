0495a444a88d83b0cc72b7a23d91d3522a77a1a4
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-17 11:21:22 -0600

    Set up describe block for queriedWord with it statements

---
+++ src/reducers/queriedWordReducer.test.js
@@ -0,0 +1,15 @@
+import { queriedWord } from './queriedWord';
+
+describe('queriedWord', () => {
+  it('should return an initial state', () => {
+
+  });
+
+  it('should return state with a new queriedWord', () => {
+
+  });
+
+  it('should replace the current value in state', () => {
+    
+  });
+})

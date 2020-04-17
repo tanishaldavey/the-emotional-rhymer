8b3d7c7716f2ece8f46a17b3e99a9633995da4fc
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-16 19:59:04 -0600

    Make fetch request to get the rhymes of recent searches

--- src/components/RecentSearches/RecentSearches.js
+++ src/components/RecentSearches/RecentSearches.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { findRhymingWords } from '../../apiCalls';
+import { getRhymes, getQueriedWord } from '../../actions';
+import { connect } from 'react-redux';
 import './RecentSearches.css';
 
-const RecentSearches = ({ recentSearch }) => {
+const RecentSearches = ({ recentSearch, getQueriedWord, getRhymes }) => {
+  const findRhymes = () => {
+    getQueriedWord(recentSearch)
+    findRhymingWords(recentSearch)
+      .then(rhymesFound => getRhymes(rhymesFound))
+  }
+
   return(
     <Link to={`/${recentSearch}/rhymes`}>
-    <li>{ recentSearch }</li>
+      <li onClick={findRhymes}>{ recentSearch }</li>
     </Link>
   )
 }
 
-export default RecentSearches;
+const mapDispatchToProps = dispatch => ({
+  getRhymes: allRhymes => dispatch( getRhymes(allRhymes) ),
+  getQueriedWord: word => dispatch( getQueriedWord(word) )
+})
+
+export default connect(null, mapDispatchToProps)(RecentSearches);

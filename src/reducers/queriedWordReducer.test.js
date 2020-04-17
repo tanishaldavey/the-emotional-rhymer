f6656fc1177d3e454004c0d96eac19ede48975ee
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-17 11:22:01 -0600

    Complete all passing tests for queriedWord reducer

--- src/reducers/queriedWordReducer.test.js
+++ src/reducers/queriedWordReducer.test.js
@@ -2,14 +2,26 @@ import { queriedWord } from './queriedWord';
 
 describe('queriedWord', () => {
   it('should return an initial state', () => {
+    const expectedResult = '';
 
+    const result = queriedWord(undefined, {})
+
+    expect(result).toEqual(expectedResult)
   });
 
   it('should return state with a new queriedWord', () => {
+    const expectedResult = 'long'
 
+    const result = queriedWord('', {type: 'GET_QUERIED_WORD', queriedWord: 'long'})
+
+    expect(result).toEqual(expectedResult)
   });
 
   it('should replace the current value in state', () => {
-    
+    const expectedResult = 'cheer'
+
+    const result = queriedWord('long', {type: 'GET_QUERIED_WORD', queriedWord: 'cheer'})
+
+    expect(result).toEqual(expectedResult)
   });
 })

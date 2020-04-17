414c879af129805c8e47d69aa24d14d05710d36c
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-16 18:51:18 -0600

    Map over recentSearches from the store to render

--- src/containers/RecentSearchesContainer/RecentSearchesContainer.js
+++ src/containers/RecentSearchesContainer/RecentSearchesContainer.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import RecentSearches from '../../components/RecentSearches/RecentSearches';
 import { connect } from 'react-redux';
 import './RecentSearchesContainer.css';
 
-const RecentSearchesContainer = () => {
+const RecentSearchesContainer = ({ recentSearches }) => {
+  let key = 0;
+  const allRecentSearches = recentSearches.map(search => {
+    return <RecentSearches
+    key={key +=1}
+    recentSearch={search}
+    />
+  })
   return(
     <section>
       <ul>
+        { allRecentSearches }
       </ul>
     </section>
   )

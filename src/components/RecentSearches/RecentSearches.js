e79e731e6264e170a85f9faaa0fbe61c87c8eb8c
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-16 18:54:19 -0600

    Render list items for each item in recentSearch

---
+++ src/components/RecentSearches/RecentSearches.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './RecentSearches.css';
+
+const RecentSearches = ({ recentSearch }) => {
+  return(
+    <li>{ recentSearch }</li>
+  )
+}
+
+export default RecentSearches;

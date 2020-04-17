6ee9d3b0f7b61cae23de6d146d8efd0aad7f87d6
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-16 19:16:27 -0600

    Add links to each word that was recently searched for quick access to their rhymes

--- src/components/RecentSearches/RecentSearches.js
+++ src/components/RecentSearches/RecentSearches.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './RecentSearches.css';
 
 const RecentSearches = ({ recentSearch }) => {
   return(
+    <Link to={`/${recentSearch}/rhymes`}>
     <li>{ recentSearch }</li>
+    </Link>
   )
 }
 

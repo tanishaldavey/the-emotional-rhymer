09b21a88fed92db3fdaefaf6f4519d89d075624a
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-17 11:59:14 -0600

    Set describe block with it statements for the recentSearch reducer

---
+++ src/reducers/recentSearchReducer.test.js
@@ -0,0 +1,15 @@
+import { recentSearch } from './recentSearch';
+
+describe('recentSearch', () => {
+  it('should return an intial state', () => {
+
+  });
+
+  it('should be able to add a new word to the array in state if there are less han 10 words in the array', () => {
+
+  });
+
+  it('should remove the last word of the array in state if there are 10 words in that array, and add the new word', () => {
+
+  });
+})

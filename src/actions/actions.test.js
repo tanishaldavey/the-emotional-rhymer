502d57478ff1cea084bc0c04a0573c73d93672da
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-17 10:19:11 -0600

    Set up describe block wtih it statements for each action type

---
+++ src/actions/actions.test.js
@@ -0,0 +1,19 @@
+import * as actions from '.';
+
+describe('actions', () => {
+  it('should have a type of GET_RHYMES', () => {
+
+  });
+
+  it('should have a type of GET_QUERIED_WORD', () => {
+
+  });
+
+  it('should have a type of GET_WORD_DETAILS', () => {
+
+  });
+
+  it('should have a type of GET_RECENT_SEARCHES', () => {
+
+  });
+});

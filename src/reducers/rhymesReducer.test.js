e4f51c8a4bce5f3ad9ff407f49f503384fdd3da1
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-17 10:49:05 -0600

    Set up describe blockw with it statements for rhymes reducer

---
+++ src/reducers/rhymesReducer.test.js
@@ -0,0 +1,11 @@
+import { rhymes } from './rhymes';
+
+describe('rhymes', () => {
+  it('should return the initial state', () => {
+
+  });
+
+  it('should return state with new rhymes', () => {
+
+  });
+})

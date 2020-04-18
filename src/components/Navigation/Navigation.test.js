8d07f728a13451e93c9c90f33eec8cc040934bac
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-17 18:11:01 -0600

    Set up describe block and it statements for testing

---
+++ src/components/Navigation/Navigation.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from './Header';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Navigation', () => {
+  it('should be able to render the correct information', () => {
+
+  });
+
+  it('should contain links to "about" and "contact"', () => {
+
+  });;
+})

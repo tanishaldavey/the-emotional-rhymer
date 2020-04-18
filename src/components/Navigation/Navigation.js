a7ba4951d90c1f8cdb781814c394c20610c4b3b0
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-17 18:10:42 -0600

    Added links (not yet routed) for about and contact

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,6 +4,14 @@ import './Navigation.css';
 const Navigation = () => {
   return(
     <nav>
+      <ul>
+      <Link>
+        <li>About</li>
+      </Link>
+      <Link>
+        <li>Contact</li>
+      </Link>
+      </ul>
     </nav>
   )
 }

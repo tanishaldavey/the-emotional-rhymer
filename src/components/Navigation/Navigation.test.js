66f3afae701228be0259ec4e4c52626cda4c6871
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-17 18:22:28 -0600

    Complete all passing tests to check that items are rendered in the Navigation correctly

--- src/components/Navigation/Navigation.test.js
+++ src/components/Navigation/Navigation.test.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import Navigation from './Header';
+import Navigation from './Navigation';
+import { BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('Navigation', () => {
   it('should be able to render the correct information', () => {
+    const { getByText } = render(<BrowserRouter>
+      <Navigation />
+    </BrowserRouter>)
 
+    const about =getByText('About')
+    const contact =getByText('Contact')
+
+    expect(about).toBeInTheDocument()
+    expect(contact).toBeInTheDocument()
   });
 
   it('should contain links to "about" and "contact"', () => {
+    const { getByRole } = render(<BrowserRouter>
+      <Navigation />
+    </BrowserRouter>)
 
+    const about =getByRole('link', {'name': 'About'})
+    const contact =getByRole('link', {'name': 'Contact'})
+
+    expect(about).toBeInTheDocument()
+    expect(contact).toBeInTheDocument()
   });;
 })

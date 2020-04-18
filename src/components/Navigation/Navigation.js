18fda0156a509c51c45c23dcdcb3f4d0a88e166a
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-17 18:18:14 -0600

    Add home route links as a placeholder for the time being

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navigation.css';
 
 const Navigation = () => {
   return(
     <nav>
       <ul>
-      <Link>
+      <Link to='/'>
         <li>About</li>
       </Link>
-      <Link>
+      <Link to='/'>
         <li>Contact</li>
       </Link>
       </ul>

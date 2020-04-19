f365fcd759aad9aee5c1ee8cd5fb0a6af60dcd99
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-19 12:58:27 -0600

    Complete passing tests for the SearchForm component

--- src/containers/SearchForm/SearchForm.test.js
+++ src/containers/SearchForm/SearchForm.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SearchForm from './SearchForm';
 import { BrowserRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -23,6 +23,25 @@ beforeEach(() => {
 
 describe('SearchForm', () => {
   it('should render the correct elements to the page', () => {
+    const { getByText, getByPlaceholderText } = testWrapper
 
+    const input = getByPlaceholderText('What word rhymes with...')
+    const submitBtn = getByText('Submit')
+    const recentSearchesBtn = getByText('Recent Searches')
+
+    expect(input).toBeInTheDocument();
+    expect(input.value).toBe('');
+    expect(submitBtn).toBeInTheDocument();
+    expect(recentSearchesBtn).toBeInTheDocument();
   });
+
+  it('should update the input\'s value when a user types into the input', () => {
+    const { getByText, getByPlaceholderText } = testWrapper
+
+    const input = getByPlaceholderText('What word rhymes with...')
+
+    fireEvent.change(input, { target: { value: 'broken' }})
+
+    expect(input.value).toBe('broken');
+  })
 });

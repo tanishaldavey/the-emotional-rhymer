ec40492110acc3e82b3d14b971f6d9aef5c9d236
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-19 14:56:42 -0600

    Complete passing test to check that rhymes are rendered to the page once submit is clicked

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from '../../reducers';
+import { findRhymingWords, findWordDetails } from '../../apiCalls';
 
+jest.mock('../../apiCalls')
 
-let testStore, testWrapper;
+let testStore, testWrapper, rhymes;
 
 beforeEach(() => {
   testStore = createStore(rootReducer)
@@ -20,10 +22,40 @@ beforeEach(() => {
       </BrowserRouter>
     </Provider>
   )
+
+  rhymes = [{
+    "word": "display",
+    "score": 2051,
+    "numSyllables": 2
+    },
+    {
+    "word": "betray",
+    "score": 1275,
+    "numSyllables": 2
+    },
+    {
+    "word": "portray",
+    "score": 1198,
+    "numSyllables": 2
+  }]
 })
 
 describe('App', () => {
-  it('should render a header to the page', () => {
+  it('should be able to search for rhymes after submit button is clicked', async () => {
 
+    findRhymingWords.mockResolvedValue(rhymes)
+
+    const { getByText, getByPlaceholderText } = testWrapper
+
+    const input = getByPlaceholderText('What word rhymes with...')
+    const submitBtn = getByText('Submit')
+
+    fireEvent.change(input, { target: {value: 'stay' }})
+    fireEvent.click(submitBtn)
+ 
+    const rhymesFound = await waitFor(() => getByText('display', 'betray', 'portray'))
+
+    expect(rhymesFound).toBeInTheDocument()
   });
+
 });

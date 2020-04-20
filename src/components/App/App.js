3f97181c5fcefbefb1ca7873515ce5ef0daab354
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-20 11:50:46 -0600

    Make changes to ensure that App tests are still passing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Navigation from '../Navigation/Navigation';
-import Header from '../Header/Header';
-import SearchForm from '../../containers/SearchForm/SearchForm'
+import HomePageContainer from '../HomePageContainer/HomePageContainer';
 import RhymesContainer from '../../containers/RhymesContianer/RhymesContainer';
 import WordDetails from '../../containers/WordDetails/WordDetails';
 import RecentSearchesContainer from '../../containers/RecentSearchesContainer/RecentSearchesContainer';
@@ -9,13 +7,11 @@ import NotFound from '../NotFound/NotFound';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <main className="App">
-      <Navigation />
-      <Header />
+      <HomePageContainer />
       <Switch>
-        <Route exact path='/' render={() => <SearchForm />} />
         <Route exact path='/:word/rhymes' render={() => <RhymesContainer />} />
         <Route path='/:word/rhymes/:otherWord/word-details' render={() => <WordDetails />} />
         <Route path='/recentSearches' render={() => <RecentSearchesContainer />} />

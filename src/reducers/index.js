6dd043fe2aba5a7a1c50a160693bbcd2fe34eb61
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-19 19:04:10 -0600

    Add error to combineReducer's function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import { rhymes } from './rhymes';
 import { queriedWord } from './queriedWord';
 import { wordDetails } from './wordDetails';
 import { recentSearches } from './recentSearch';
+import { error } from './error';
 
 const rootReducer = combineReducers({
   rhymes,
   queriedWord,
   wordDetails,
-  recentSearches
+  recentSearches,
+  error
 });
 
 export default rootReducer;

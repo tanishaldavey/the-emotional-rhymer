ec3fdce56388fe6b84e21366c1bef28d3b8a3e13
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-19 20:34:01 -0600

    Remove error on change of input

--- src/containers/SearchForm/SearchForm.js
+++ src/containers/SearchForm/SearchForm.js
@@ -17,7 +17,7 @@ class SearchForm extends Component {
 
   updateValue = e => {
     this.setState({ [e.target.name]: e.target.value })
-    // this.setState({ error: '' })
+    this.setState({ error: '' })
   }
 
   validateForm = e => {

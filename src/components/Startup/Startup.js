6dd29c98b6bdce046784637149aad6f15c2ed73f
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-27 19:55:30 -0600

    Add startup component to render on routing to home page

---
+++ src/components/Startup/Startup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Startup.scss';
+
+const Startup = () => {
+    return (
+        <section>
+            <p>Begin by entering a word to search for in the input.</p>
+        </section>
+    )
+}
+
+export default Startup;

ec553b83095b48001bb23e339c1162d6d3e637ff
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-05-10 16:57:01 -0600

    Add updateLoadingStatus to submit function

--- src/containers/SearchForm/SearchForm.js
+++ src/containers/SearchForm/SearchForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { findRhymingWords } from '../../apiCalls';
-import { getRhymes, getQueriedWord, getRecentSearches, updateError } from '../../actions';
+import { getRhymes, getQueriedWord, getRecentSearches, updateError, udpateLoadingStatus } from '../../actions';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './SearchForm.scss';
@@ -39,9 +39,11 @@ class SearchForm extends Component {
           this.props.updateError({ data }.data)
         } else {
           this.props.getRhymes(data)
+          this.props.udpateLoadingStatus(true)
         }
       })
       .then(this.clearInput())
+      .then(this.props.udpateLoadingStatus(false))
       .catch(error => console.log(error.message))
   }
 
@@ -108,7 +110,8 @@ const mapDispatchToProps = dispatch => ({
   getQueriedWord: word => dispatch( getQueriedWord(word) ),
   getRecentSearches: recentSearchValue =>
     dispatch( getRecentSearches(recentSearchValue) ),
-  updateError: error => dispatch( updateError(error) )
+  updateError: error => dispatch( updateError(error) ),
+  udpateLoadingStatus: status => dispatch( udpateLoadingStatus(status) )
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchForm);
@@ -118,5 +121,6 @@ SearchForm.propTypes = {
   getRhymes: PropTypes.func,
   getQueriedWord: PropTypes.func,
   getRecentSearches: PropTypes.func,
-  updateError: PropTypes.func
+  updateError: PropTypes.func,
+  udpateLoadingStatus: PropTypes.func
 }
